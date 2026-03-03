06f96eeac625de38f2e2926ef571b4dd74a8bc70
Tara R <baracat15@gmail.com>
2026-03-03 23:17:26 +0000

    Added 2 movies through admin, now fixing faulty 'Book Seats' button

--- homework2/movie_theater_booking/movie_theater_booking/settings.py
+++ homework2/movie_theater_booking/movie_theater_booking/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +129,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://app-hw-21.devedu.io',
+    'https://*.devedu.io',
+]
+
+
+CSRF_TRUSTED_ORIGINS.append('http://app-hw-21.devedu.io')
